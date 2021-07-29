9b84f43c0e5a50467e4be2f45add6d7592cdf622
Pawel33359 <79919268+Pawel33359@users.noreply.github.com>
2021-07-29 19:50:13 +0200

    Add files via upload

---
+++ ninja-jobs/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro ratione velit placeat nisi eveniet ipsam laboriosam atque assumenda corrupti quidem beatae rem, neque exercitationem unde nihil, et incidunt eaque. Rerum!</p>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro ratione velit placeat nisi eveniet ipsam laboriosam atque assumenda corrupti quidem beatae rem, neque exercitationem unde nihil, et incidunt eaque. Rerum!</p>
+  </div>
+</template>
---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   <h1>Homepage</h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro ratione velit placeat nisi eveniet ipsam laboriosam atque assumenda corrupti quidem beatae rem, neque exercitationem unde nihil, et incidunt eaque. Rerum!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro ratione velit placeat nisi eveniet ipsam laboriosam atque assumenda corrupti quidem beatae rem, neque exercitationem unde nihil, et incidunt eaque. Rerum!</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
